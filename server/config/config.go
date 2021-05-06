662c23c060083936966e9036481bfa4ad2f3add9
Buterin <admin@example.com>
2021-05-06 10:19:18 +0800

    fix defaultMinGasPrices 0.25 to 0.025

--- server/config/config.go
+++ server/config/config.go
@@ -9,7 +9,7 @@ import (
 )
 
 const (
-	defaultMinGasPrices = "0.25" + sdk.DefaultBondDenom
+	defaultMinGasPrices = "0.025" + sdk.DefaultBondDenom
 )
 
 // BaseConfig defines the server's basic configuration
